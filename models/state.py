85361efd9825a9049c62759563c3e793dc8c14ca
esianyo <esianicd@gmail.com>
2023-11-22 20:56:16 +0000

    my weirdest commit message...lol

--- models/state.py
+++ models/state.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.city import City
+import models
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
+
+    @property
+    def cities(self):
+        """
+        getter attribute cities that returns the list of City
+        instances with state_id equals to the current State.id
+        """
+        return [city for city in models.storage.all(City).values()
+                if city.state_id == self.id]
