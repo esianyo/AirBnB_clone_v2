be334a0d9077b83c64d83fdae532089530815cb4
esianyo <esianicd@gmail.com>
2023-12-31 13:14:37 +0000

    dubile

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,34 +1,32 @@
 #!/usr/bin/python3
 """a script that starts a Flask web application"""
 
-from flask import Flask
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
-    return "Hello HBNB!"
+    return 'Hello HBNB!'
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return "HBNB"
+    return 'HBNB'
 
 
-@app.route("/c/<text>", strict_slashes=False)
-def c_route(text):
-    processed_text = text.replace("_", " ")
-    return f"C {processed_text}"
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    return 'C {}'.format(text.replace('_', ' '))
 
 
-@app.route("/python/<text>", defaults={"text": "is cool"},
+@app.route('/python/', defaults={'text': 'is cool'},
            strict_slashes=False)
-def python_route(text):
-    processed_text = text.replace("_", " ")
-    return f"Python {processed_text}"
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    return 'Python {}'.format(text.replace('_', ' '))
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
