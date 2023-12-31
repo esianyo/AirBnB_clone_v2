493408876e3c38fb342f02d96c13aa8541b9beb4
esianyo <esianicd@gmail.com>
2023-12-31 13:53:41 +0000

    dubile

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -40,7 +40,8 @@ def number_template(n):
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
-    return render_template('6-number_odd_or_even.html', number=n, result="odd" if n % 2 != 0 else "even")
+    return render_template('6-number_odd_or_even.html',
+                           number=n, result="odd" if n % 2 != 0 else "even")
 
 
 if __name__ == '__main__':
